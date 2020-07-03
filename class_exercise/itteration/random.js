b20fda28640dfee0eb95cd9aa30f75cb6c4c89c2
idonia <sidoniag@gmail.com>
2020-07-03 16:06:15 -0500

    inital files

---
+++ class_exercise/itteration/random.js
@@ -0,0 +1,30 @@
+//Create a function, adder(), that accepts three arguments and returns their sum.
+
+function myFunction (addNumber)
+
+console.log (numbers);
+
+var numbers + var numbers (a, b, c) {
+    return a + b + c;
+}
+
+var x = myFunction(1,2,3);
+//Create a function, muliplier(), that accepts three arguments and returns their product.
+
+//Create a function, isString(), that accepts three arguments and checks if the type of all three is string.
+
+//Create a function, vowelCheker(), that accepts one argument, a string, and checks if the first letter of the string is a vowel.
+
+// This is a Number data type; it can be an integer (whole number) or have decimals (floated numbers).
+var numberIntegerDataType = 10;
+var numberFloatDataType = 10.4;
+
+// This is a String data type; it must be wrapped in double quotes (" ") or single quotes (' ').
+var stringDataType = "This is a string, which is a fancy way to say text";
+
+
+// This is a Boolean data type, which can only be given a value of true or false.
+var booleanDataType = true;
+
+// lets me know its working
+window.alert("Function");
