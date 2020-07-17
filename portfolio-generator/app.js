27ae71d09edf7e8dcf0cc174f064c572c166f5ff
idonia <sidoniag@gmail.com>
2020-07-17 15:56:45 -0500

    user data start

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -1,14 +1,28 @@
+// holds user command-line arguements
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('===================');
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('===================');
 
-    profileDataArr.forEach(profileItem =>
-        console.log(profileItem));
+//     profileDataArr.forEach(profileItem =>
+//         console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+// extract arguements and store them in distinct variables
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
+const generatePage = (userName, githubName) => {
+    return `
+        Name: ${userName}
+        Github: ${githubName}
+    `;
 };
 
-printProfileData(profileDataArgs);
+console.log(generatePage(name, github));
