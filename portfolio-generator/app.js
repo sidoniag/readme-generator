f98bacd37e82324990c5d96e3aac32f02dad5b76
idonia <sidoniag@gmail.com>
2020-07-17 16:30:59 -0500

    modularized

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -1,44 +1,14 @@
 // required in order to use fs module
 const fs = require('fs');
 
+// link to page-template.js
+const generatePage = require('./src/page-template.js');
+
 // holds user command-line arguements
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('===================');
-
-//     profileDataArr.forEach(profileItem =>
-//         console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
 
 // extract arguements and store them in distinct variables
 const [name, github] = profileDataArgs;
-
-// generate HTML string
-const generatePage = (name, github) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
-
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
 
 // use fs.writeFile() function to create html
 fs.writeFile('index.html', generatePage(name, github), err => {
--- portfolio-generator/src/page-template.js
+++ portfolio-generator/src/page-template.js
@@ -1,4 +1,6 @@
-
+// generate HTML string
+const generatePage = (name, github) => {
+    return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -9,8 +11,11 @@
     </head>
 
     <body>
-        <h1>jane</h1>
-        <h2><a href="https://github.com/undefined">Github</a></h2>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
     </body>
     </html>
-    
+    `;
+};
+
+module.exports = generatePage;
