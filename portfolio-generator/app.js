a1e2e42566fab135dea754991bd1b9a2487dfca6
idonia <sidoniag@gmail.com>
2020-07-17 16:04:39 -0500

    html template

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -15,14 +15,27 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 // printProfileData(profileDataArgs);
 
 // extract arguements and store them in distinct variables
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-const generatePage = (userName, githubName) => {
+// generate HTML string
+const generatePage = (name, github) => {
     return `
-        Name: ${userName}
-        Github: ${githubName}
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
     `;
 };
 
+console.log(name, github);
 console.log(generatePage(name, github));
