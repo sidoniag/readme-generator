b46807646cf77e27bc3c6a8cca60d5c99bb08300
idonia <sidoniag@gmail.com>
2020-07-20 16:08:45 -0500

    node modules add to .gitignore

---
+++ readme-generator/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+// require inquire
+
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw (err);
+        console.log('ReadMe.md complete!');
+    });
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
---
+++ readme-generator/utils/generateMarkdown.js
@@ -0,0 +1,11 @@
+
+// function to generate markdown for README
+function generateMarkdown(data) {
+    return `
+    # ${data.title}
+    # ${}
+
+  `;
+  }
+  
+  module.exports = generateMarkdown;
