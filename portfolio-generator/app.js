01ec44cc6617bbd03fe25b2a1fb9fcc152350c24
idonia <sidoniag@gmail.com>
2020-07-20 16:13:55 -0500

    require inquirer

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -1,13 +1,16 @@
 // required in order to use fs module
 const fs = require('fs');
 
+// use inquirer
+const inquirer = require('inquirer');
+
 // link to page-template.js
 const generatePage = require('./src/page-template.js');
 
-// holds user command-line arguements
+// holds user command-line arguments
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-// extract arguements and store them in distinct variables
+// extract arguments and store them in distinct variables
 const [name, github] = profileDataArgs;
 
 // use fs.writeFile() function to create html
