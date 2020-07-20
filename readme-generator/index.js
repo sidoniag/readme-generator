8d533824779b2200a372d30dbc7b7152390e2cbd
idonia <sidoniag@gmail.com>
2020-07-20 15:30:45 -0500

    link generateMarkdown.js

--- readme-generator/index.js
+++ readme-generator/index.js
@@ -1,3 +1,9 @@
+// use fs module
+const fs = require('fs');
+
+// link to generatemarkdown.js
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // array of questions for user
 const questions = [
 
@@ -6,6 +12,12 @@ const questions = [
 // function to write README file
 function writeToFile(fileName, data) {
 }
+
+fs.writeFile('readme.md', generateMarkdown(data), err => {
+    if (err) throw err;
+
+    console.log('ReadMe complete! Checkout ReadMe.md to see the output.');
+});
 
 // function to initialize program
 function init() {
