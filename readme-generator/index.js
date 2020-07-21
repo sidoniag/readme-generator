3f416b5b28b1bf70dd8710962e5486e864b298a4
idonia <sidoniag@gmail.com>
2020-07-20 19:41:06 -0500

    setup questions

--- readme-generator/index.js
+++ readme-generator/index.js
@@ -5,13 +5,55 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // array of questions for user
 const promptUser = () => {
     return inquirer.prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'What is your name?'
-            }
-        ]);
-    };    
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address'
+        },
+        {
+            type: 'input',
+            name: 'project',
+            message: 'Enter your project name'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description about the project'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license should the project have?',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3']
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions on how to use your project'
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'Provide description on how to contribute to your projects'
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What comman should be run to install dependencies?',
+            default: 'npm i'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What command should be run to run tests?'
+        }
+    ]);
+};    
     promptUser().then(answers => console.log(answers));
 
 // ];
