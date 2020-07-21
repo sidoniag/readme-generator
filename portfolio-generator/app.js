9b8698ddc5b6ec1f61d8a8aca8e9257d6b5a0cac
idonia <sidoniag@gmail.com>
2020-07-20 19:29:28 -0500

    initial questions

--- portfolio-generator/app.js
+++ portfolio-generator/app.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 // use inquirer
 const inquirer = require('inquirer');
 
-console.log(inquirer);
-
 // link to page-template.js
 const generatePage = require('./src/page-template.js');
 
@@ -15,9 +13,21 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 // extract arguments and store them in distinct variables
 const [name, github] = profileDataArgs;
 
-// use fs.writeFile() function to create html
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// // use fs.writeFile() function to create html
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+// questions
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
