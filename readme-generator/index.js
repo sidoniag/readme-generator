88b397b0e61be617af6204eb93365a8d0a2d8b6c
idonia <sidoniag@gmail.com>
2020-07-20 19:31:48 -0500

    begin question prompts

--- readme-generator/index.js
+++ readme-generator/index.js
@@ -3,16 +3,16 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // array of questions for user
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        }
-    ])
-    .then(answers => console.log(answers));
-// const questions = [
+const promptUser = () => {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            }
+        ]);
+    };    
+    promptUser().then(answers => console.log(answers));
 
 // ];
 
